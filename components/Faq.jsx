b5aa029457a3df5dcf5767a0f4efcc0763fae634
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2025-01-07 20:28:16 +0530

    ✨ ✨ Initial commit

---
+++ components/Faq.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const Faq = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Faq;
---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        dmSans: ['DM Sans', 'sans-serif']
+      },
+      colors: {
+        bright_green: "#0DFF4E",
+        event_gray: "#202020",
+        bg_black: "#0B0F12"
+      },
+    },
+  },
+  plugins: [],
+};
