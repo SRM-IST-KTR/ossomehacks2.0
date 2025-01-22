fbe7117c056ecf0bf69ded5de4b3c734ae226d52
nostack93 <coteforreal@gmail.com>
2025-01-22 23:15:10 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Head from 'next/head';
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
 
 export default function Hero() {
   const [timeLeft, setTimeLeft] = useState({
@@ -12,48 +10,51 @@ export default function Hero() {
     seconds: '00',
   });
 
-  const particlesInit = useCallback(async engine => {
-    await loadFull(engine);
-  }, []);
-
-  const particlesConfig = {
-    particles: {
-      number: { value: 160, density: { enable: true, value_area: 800 } },
-      color: { value: "#ffffff" },
-      shape: { type: "circle" },
-      opacity: {
-        value: 1,
-        random: true,
-        anim: { enable: true, speed: 1, opacity_min: 0 },
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: { enable: false },
-      },
-      move: {
-        enable: true,
-        speed: 1,
-        direction: "none",
-        random: true,
-        out_mode: "out",
-      },
-    },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: { enable: true, mode: "bubble" },
-        onclick: { enable: true, mode: "repulse" },
-      },
-      modes: {
-        bubble: { distance: 250, size: 0, duration: 2 },
-        repulse: { distance: 400, duration: 0.4 },
-      },
-    },
-    retina_detect: true,
-  };
-
   useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      window.particlesJS('particles-js', {
+        particles: {
+          number: { value: 160, density: { enable: true, value_area: 800 } },
+          color: { value: "#ffffff" },
+          shape: { type: "circle" },
+          opacity: {
+            value: 1,
+            random: true,
+            anim: { enable: true, speed: 1, opacity_min: 0 },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: { enable: false },
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            random: true,
+            out_mode: "out",
+          },
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: { enable: true, mode: "bubble" },
+            onclick: { enable: true, mode: "repulse" },
+          },
+          modes: {
+            bubble: { distance: 250, size: 0, duration: 2 },
+            repulse: { distance: 400, duration: 0.4 },
+          },
+        },
+        retina_detect: true,
+      });
+    };
+
     const targetDate = new Date();
     targetDate.setDate(targetDate.getDate() + 25);
 
@@ -76,69 +77,45 @@ export default function Hero() {
     };
 
     const interval = setInterval(updateTimer, 1000);
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
-    <div className="relative min-h-screen bg-[#1E002E]">
-      {/* Particles Background */}
-      <div className="absolute inset-0">
-        <Particles id="particles-js" init={particlesInit} options={particlesConfig} />
-      </div>
+    <div className="relative min-h-screen bg-[#1E002E] px-4 md:px-0">
+      <div id="particles-js" className="absolute inset-0" />
 
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@800&display=swap" rel="stylesheet" />
       </Head>
 
-      {/* Content Overlay */}
       <div className="relative z-10">
-        {/* Scrolling Registration Text */}
         <div className="relative w-full overflow-hidden">
           <div className="flex whitespace-nowrap animate-infinite-scroll font-sans text-[16px] font-extrabold text-[#FCF961]">
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-            <div className="flex shrink-0">
-              REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
-            </div>
-
-
-
+            {Array(7).fill(
+              <div className="flex shrink-0">
+                REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN · REGISTRATION IS NOW OPEN ·
+              </div>
+            )}
           </div>
         </div>
 
-        {/* Heading */}
-        <div className="pt-[212px] flex justify-center">
+        <div className="pt-[212px] flex justify-center px-4 md:px-0">
           <Image src="/hero/ossome-hacks-heading.svg" width={1120} height={168} alt="Heading" />
         </div>
 
-        {/* Subheading */}
-        <div className="pt-[30px] flex justify-center ml-[-90px] mt-[-35px]">
+        <div className="pt-[30px] flex justify-center ml-[-90px] mt-[-35px] px-4 md:px-0">
           <Image src="/hero/s1.svg" width={1440} height={32} alt="Subheading" />
         </div>
 
-        {/* Buttons */}
-        <div className="flex justify-center pt-[70px] gap-[20px] flex-wrap sm:flex-nowrap">
+        <div className="flex justify-center pt-[70px] gap-[20px] flex-wrap sm:flex-nowrap px-4 md:px-0">
           <button className="text-white font-semibold w-[250px] sm:w-[250px] h-[50px] rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition duration-200">
             <Image src="/hero/b1.svg" layout="intrinsic" width={250} height={50} alt="Register" />
           </button>
 
-          <button className="border-4 border-[#FCF961] text-white font-semibold w-[250px] sm:w-[250px] h-[50px] rounded-[10px] hover:bg-[#FCF961] hover:text-white text-[16px] font-Uni Sans Heavy flex items-center justify-center">
+          <button className="border-4 border-[#FCF961] text-white font-semibold w-[250px] sm:w-[250px] h-[50px] rounded-[10px] hover:bg-[#FCF961] hover:text-black text-[16px] font-Uni Sans Heavy flex items-center justify-center">
             <div style={{ marginRight: "8px", marginTop: "2.7px" }}>
               <Image src="/hero/discord_icon.svg" width={18} height={15} alt="Discord Icon" />
             </div>
@@ -146,32 +123,20 @@ export default function Hero() {
           </button>
         </div>
 
-        {/* Countdown Timer */}
-        <div className="flex justify-center pt-[68px] space-x-[20px]">
-          <div className="text-center text-white font-semibold text-3xl font-sans">
-            <div style={{ fontSize: '1.5rem' }}>
-              <span className="font-inter">{timeLeft.days}</span>
+        <div className="flex justify-center pt-[68px] space-x-[20px] px-4 md:px-0">
+          {[
+            { value: timeLeft.days, label: 'Days' },
+            { value: timeLeft.hours, label: 'Hours' },
+            { value: timeLeft.minutes, label: 'Minutes' },
+            { value: timeLeft.seconds, label: 'Seconds' }
+          ].map((item, index) => (
+            <div key={index} className="text-center text-white font-semibold text-3xl font-sans">
+              <div style={{ fontSize: '1.5rem' }}>
+                <span className="font-inter">{item.value}</span>
+              </div>
+              <div style={{ marginTop: '5px' }} className="text-sm uppercase tracking-widest font-sans">{item.label}</div>
             </div>
-            <div style={{ marginTop: '5px' }} className="text-sm uppercase tracking-widest font-sans">Days</div>
-          </div>
-          <div className="text-center text-white font-semibold text-3xl font-sans">
-            <div style={{ fontSize: '1.5rem' }}>
-              <span className="font-inter">{timeLeft.hours}</span>
-            </div>
-            <div style={{ marginTop: '5px' }} className="text-sm uppercase tracking-widest font-sans">Hours</div>
-          </div>
-          <div className="text-center text-white font-semibold text-3xl font-sans">
-            <div style={{ fontSize: '1.5rem' }}>
-              <span className="font-inter">{timeLeft.minutes}</span>
-            </div>
-            <div style={{ marginTop: '5px' }} className="text-sm uppercase tracking-widest font-sans">Minutes</div>
-          </div>
-          <div className="text-center text-white font-semibold text-3xl font-sans">
-            <div style={{ fontSize: '1.5rem' }}>
-              <span className="font-inter">{timeLeft.seconds}</span>
-            </div>
-            <div style={{ marginTop: '5px' }} className="text-sm uppercase tracking-widest font-sans">Seconds</div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
