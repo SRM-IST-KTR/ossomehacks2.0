2e573ccd42f9d0440fb6c93979f62a4c9abce953
Anushka Das <iamanushkadas@gmail.com>
2025-02-01 23:50:38 +0530

    Fixed font sizes

--- components/About.jsx
+++ components/About.jsx
@@ -15,8 +15,8 @@ const About = () => {
                     <p className='text-[#FCF961] text-center sm:text-[60px] lg:text-[140px] md:text-[100px] text-[30px] font-raleway font-extrabold -tracking-tight lg:-mt-[25%] -mt-[26%] '>ABOUT</p>
                     </div>
                     </div>
-                    <p className='text-[#AA2490] font-bold font-poppins lg:text-[32px] md:text-[24px] text-[16px] pb-3 '>Code Your Passion, Build Your Legacy.</p>
-                    <p className='text-white lg:text-[24px] md:text-[16px] text-[9px] lg:w-[920px] md:w-[620px] w-[360px] '>
+                    <p className='text-[#AA2490] font-bold font-poppins lg:text-[32px] sm:text-[24px] md:text-[24px] text-[20px] pb-3 '>Code Your Passion, Build Your Legacy.</p>
+                    <p className='text-white lg:text-[24px] sm:text-[20px] md:text-[16px] text-[15px] lg:w-[920px] md:w-[620px] w-[360px] '>
                         “<span className='font-bold'> Ossome Hacks 2.0 </span> builds on the success of its inaugural edition in September 2023, which saw 250+ participants in 50+ teams solving real-world challenges. This <span className='font-bold'>36-hour hackathon</span>  engages tech enthusiasts of all levels with diverse tracks, fostering innovation, skill-building, and collaboration. More than a competition, it celebrates 
                         open-source development and empowers participants to make a meaningful impact.”
                     </p>
