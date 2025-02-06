f4391d106aef6596d12fa13cb623f4fabe5c7fe7
Anushka Das <iamanushkadas@gmail.com>
2025-02-07 00:41:45 +0530

    Fixed partners mobile view and added gallery

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Gallery = () => {
     return (
         <div>
-
-        </div>
+        <p className="text-[#FCF961] text-center sm:text-[120px] lg:text-[280px] md:text-[230px] text-[60px] -ml-2 font-raleway font-extrabold -tracking-widest">
+        GLIMPSE
+      </p>
+      <div className="flex items-start justify-center">
+        <p className="text-[#6B5E77] text-center sm:text-[60px] lg:text-[140px] md:text-[100px] text-[30px] font-raleway font-extrabold -tracking-tight lg:ml-5 lg:-mt-[15%] -mt-[13%]">
+          GLIMPSE
+        </p>
+      </div>
+      </div>
+        
     )
 }
 
